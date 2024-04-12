c06e131ebec5493a068e668cff1b987742dfcf36
Luan Silva <luan_barbosa_da_silva@outlook.com>
2024-04-12 02:23:08 -0300

    up

--- empresaConfig.js
+++ empresaConfig.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 
 const getConfigById = async () => {
   try {
-    return await prisma.tenants.findMany();
+    return await prisma.tenant.findMany();
   } catch (error) {
     console.error('Erro ao obter configurações das empresas:', error);
     throw error;
