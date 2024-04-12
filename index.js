e13f01a4e7f0667c707dbc492602a59e17b6cee1
Luan Silva <luan_barbosa_da_silva@outlook.com>
2024-04-12 02:20:19 -0300

    Up

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 require('dotenv').config({ path: './.env.producao' });
 
-const https = require('https');
-const fs = require('fs');
-const app = require('./app');
-const { createWebhook } = require('./lib/pix');
-const { getConfigById } = require('./empresaConfig');
-const remoteCertURL = 'https://pix.gerencianet.com.br/webhooks/chain-pix-prod.crt';
+// const https = require('https');
+// const fs = require('fs');
+// const app = require('./app');
+// const { createWebhook } = require('./lib/pix');
+// const { getConfigById } = require('./empresaConfig');
+// const remoteCertURL = 'https://pix.gerencianet.com.br/webhooks/chain-pix-prod.crt';
 
-// Função para baixar o certificado remoto
-function downloadRemoteCertificate(remoteURL, callback) {
-  https.get(remoteURL, (response) => {
-    if (response.statusCode !== 200) {
-      callback(new Error('Falha ao baixar o certificado remoto'), null);
-      return;
-    }
+// // Função para baixar o certificado remoto
+// function downloadRemoteCertificate(remoteURL, callback) {
+//   https.get(remoteURL, (response) => {
+//     if (response.statusCode !== 200) {
+//       callback(new Error('Falha ao baixar o certificado remoto'), null);
+//       return;
+//     }
 
-    let certData = '';
+//     let certData = '';
 
-    response.on('data', (chunk) => {
-      certData += chunk;
-    });
+//     response.on('data', (chunk) => {
+//       certData += chunk;
+//     });
 
-    response.on('end', () => {
-      callback(null, certData);
-    });
-  }).on('error', (error) => {
-    callback(error, null);
-  });
-}
-
-// Baixe o certificado remoto e configure as opções (certificados gerados no certbot)
-downloadRemoteCertificate(remoteCertURL, (error, certData) => {
-  if (error) {
-    console.error('Erro ao baixar o certificado remoto:', error);
-    return;
-  }
-
-  const options = {
-    // TLS
-    key: fs.readFileSync('/etc/letsencrypt/live/api-pix.lsdeveloper.com.br/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api-pix.lsdeveloper.com.br/fullchain.pem'),
-
-    // MTLS (usando o certificado remoto)
-    ca: certData, 
-    minVersion: 'TLSv1.2',
-    requestCert: true,
-    rejectUnauthorized: false,
-  };
-
-  const server = https.createServer(options, app);
-  server.listen(443, async () => {
-    const tenants = await getConfigById();
-    createWebhook(tenants).then(() => {
-      console.log('Webhook criado com sucesso.');
-    }).catch((err) => {
-      console.error('Erro ao criar webhook:', err);
-    });
-  });
-});
-
-// const pegar = async () => {
-//   const tenants = await getConfigById();
-//   return tenants;
+//     response.on('end', () => {
+//       callback(null, certData);
+//     });
+//   }).on('error', (error) => {
+//     callback(error, null);
+//   });
 // }
 
-// pegar().then(result => console.log(result)).catch(error => console.error(error));
+// // Baixe o certificado remoto e configure as opções (certificados gerados no certbot)
+// downloadRemoteCertificate(remoteCertURL, (error, certData) => {
+//   if (error) {
+//     console.error('Erro ao baixar o certificado remoto:', error);
+//     return;
+//   }
+
+//   const options = {
+//     // TLS
+//     key: fs.readFileSync('/etc/letsencrypt/live/api-pix.lsdeveloper.com.br/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/api-pix.lsdeveloper.com.br/fullchain.pem'),
+
+//     // MTLS (usando o certificado remoto)
+//     ca: certData, 
+//     minVersion: 'TLSv1.2',
+//     requestCert: true,
+//     rejectUnauthorized: false,
+//   };
+
+//   const server = https.createServer(options, app);
+//   server.listen(443, async () => {
+//     const tenants = await getConfigById();
+//     createWebhook(tenants).then(() => {
+//       console.log('Webhook criado com sucesso.');
+//     }).catch((err) => {
+//       console.error('Erro ao criar webhook:', err);
+//     });
+//   });
+// });
+
+const pegar = async () => {
+  const tenants = await getConfigById();
+  return tenants;
+}
+
+pegar().then(result => console.log(result)).catch(error => console.error(error));
