a59b10ef794234b74ac84b8b88372a8c5d097744
Luan Silva <luan_barbosa_da_silva@outlook.com>
2024-04-12 01:18:20 -0300

    Database conection
    Conectando a API ao banco de dados para pegar os dados das lojas direto do banco e criar os webhooks de forma dinâmica.

---
+++ empresaConfig.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const getConfigById = async () => {
+  try {
+    return await prisma.tenant.findMany();
+  } catch (error) {
+    console.error('Erro ao obter configurações das empresas:', error);
+    throw error;
+  }
+}
+
+module.exports = {
+  getConfigById
+};
